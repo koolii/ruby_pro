4a8664745c974ffbb237a19885ed2ea527da566a
koolii <kuriyama@createentrance.com>
2018-03-28 13:19:34 +0200

    :+1: fin chapter 4.4

--- src/chapter04.rb
+++ src/chapter04.rb
@@ -44,3 +44,51 @@ p sum
 sum = 0
 [1,2,3,4].each { |n| sum += n }
 p sum
+
+# 4.4
+# Map
+new_numbers = []
+[1,2,3,4,5].each { |n| new_numbers << n * 10 }
+p new_numbers
+
+new_numbers = [1,2,3,4,5].map { |n| n * 10 }
+p new_numbers
+
+# Select
+even_numbers = [1,2,3,4,5].select { |n| n.even? }
+p even_numbers
+
+non_multiples_of_three = [1,2,3,4,5].reject { |n| n %3 == 0 }
+p non_multiples_of_three
+
+# Find
+# jsで言うところの[].some()で、最初にtrueになった値を返す
+even_number = [1,2,3,4,5,6].find { |n| n.even? }
+p even_number
+
+# Inject
+sum = 0
+[1,2,3,4].each { |n| sum += n }
+p sum
+
+# ↑の処理はinjectで書くと
+# resultはinject()の第一引数が割り当てられる
+sum = [1,2,3,4].inject(0) { |result, n| result + n }
+p sum
+sum = [1,2,3,4].inject(10) { |result, n| result + n }
+p sum
+
+
+# &とシンボルで簡潔に
+# * ブロック引数(今回だとs,n)が1つだけで
+# * 呼び出すメソッドに引数がない
+# * 一度だけメソッドを呼出して終了する
+p ['ruby', 'java', 'perl'].map { |s| s.upcase }
+p ['ruby', 'java', 'perl'].map(&:upcase)
+
+p [1,2,3,4,5].select { |n| n.odd? }
+p [1,2,3,4,5].select(&:odd?)
+
+# こういう↓ものは簡潔にはかけない
+p [1,2,3,4,5,6].select { |n| n % 3 == 0 }
+p [9, 10, 11, 12].map { |n| n.to_s(16) }
