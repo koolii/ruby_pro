9f06ac7c2ab3afc7a38475ddff77a59f2d0cfdb7
koolii <kuriyama@createentrance.com>
2018-03-30 18:00:07 +0200

    add rgb program refactoring

--- ruby-book/lib/rgb.rb
+++ ruby-book/lib/rgb.rb
@@ -1,7 +1,7 @@
 # rjust()は0埋めすることが出来るutily関数
 def to_hex(r, g, b)
-  '#' +
-  r.to_s(16).rjust(2, '0') +
-  g.to_s(16).rjust(2, '0') +
-  b.to_s(16).rjust(2, '0')
+  [r, g, b].inject('#') do |hex, n|
+    # hexには初期値が'#'となり、ループが進むに連れて、結果が追加されていく事を認識することが大事
+    hex + n.to_s(16).rjust(2, '0')
+  end
 end
