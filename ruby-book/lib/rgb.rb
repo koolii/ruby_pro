b50cde6a4a894fd730a367fb846437f439de2cfb
koolii <koolii0909@gmail.com>
2018-04-01 05:03:42 +0200

    :+1: add to_ints method

--- ruby-book/lib/rgb.rb
+++ ruby-book/lib/rgb.rb
@@ -5,3 +5,14 @@ def to_hex(r, g, b)
     hex + n.to_s(16).rjust(2, '0')
   end
 end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  ints = []
+  [r, g, b].map do |n|
+    ints << n.hex
+  end
+  ints
+end
