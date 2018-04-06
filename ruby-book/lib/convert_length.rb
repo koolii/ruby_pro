af02bc4e96b90d52e6b328b3b1e9f069afaf23c4
koolii <kuriyama@createentrance.com>
2018-04-06 13:27:10 +0200

    :+1: fin convert_length

--- ruby-book/lib/convert_length.rb
+++ ruby-book/lib/convert_length.rb
@@ -1,5 +1,7 @@
 def convert_length(length, unit_from, unit_to)
   # メートル、フィート、インチの単位
+  # 1メートルをインチに治す => 1 / 1.0 * 39.37
+  # 1フィートをメートルに治す => 1 / 3.28 * 1.0
   units = { 'm' => 1.0, 'ft' => 3.28, 'in' => 39.37 }
-  39.37
+  (length / units[unit_from] * units[unit_to]).round(2)
 end
