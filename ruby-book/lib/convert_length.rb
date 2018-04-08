9c06b8d8144b6f4e3bb58e761b6a408e6b8e4a70
koolii <kuriyama@createentrance.com>
2018-04-08 23:59:59 +0100

    refactor convert_length

--- ruby-book/lib/convert_length.rb
+++ ruby-book/lib/convert_length.rb
@@ -1,7 +1,9 @@
-def convert_length(length, unit_from, unit_to)
+UNITS = { m: 1.0, ft: 3.28, in: 39.37 }
+
+# todo キーワード引数の使い方がちょっと理解できていない気がする
+def convert_length(length, from: :m, to: :m)
   # メートル、フィート、インチの単位
   # 1メートルをインチに治す => 1 / 1.0 * 39.37
   # 1フィートをメートルに治す => 1 / 3.28 * 1.0
-  units = { 'm' => 1.0, 'ft' => 3.28, 'in' => 39.37 }
-  (length / units[unit_from] * units[unit_to]).round(2)
+  (length / UNITS[from] * UNITS[to]).round(2)
 end
