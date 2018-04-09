05c0a0f03318c50ffb17cdb4834c8488453d83d4
koolii <kuriyama@createentrance.com>
2018-04-09 13:43:26 +0200

    :+1: fin 5.7

--- src/chapter05.rb
+++ src/chapter05.rb
@@ -29,3 +29,41 @@ def buy_burger(menu, drink:, potato:)
 end
 buy_burger('menu', drink: true, potato: true)
 buy_burger('menu', potato: false, drink: false)
+
+# 5.6
+
+# jsのオブジェクトの展開がRubyでは **で表現
+h = { us: 'doller', india: 'rupee' }
+h = { japan: 'yen', **h }
+p h
+
+# jsと同様に一番最後の引数をハッシュにしたメソッドはいくらでも配置できる
+def buy_burger(menu, drink: true, potato: true, **others)
+  puts others
+end
+
+buy_burger('fish', drink: true, potato: false, salad: true, chicken: false)
+
+
+def buy_burger2(menu, options = {})
+  puts options
+end
+# 最後の引数がハッシュなら呼出し元は{}を省略できる(ただし、最後でなければ{}が必須)
+buy_burger2('fish', 'drink' => true, 'potato' => false)
+
+# Rubyでのメソッド呼び出しで()を省略するときの注意点は第一引数がハッシュだと()無しだと{}がブロックだと判断されてしまいエラーとなる
+# なので第二引数以降がハッシュなら()を省略することが出来る
+
+buy_burger2 'fish', { drink: true, potato: false }
+buy_burger2 'fish', drink: true, potato: false
+
+# hash <=> array
+p [[:japan, 'yen'], [:japan, '円']].to_h
+p [[:japan, 'yen'], [:us, 'doller'], [:india, 'rupee']].to_h
+
+# ハッシュの初期化は配列の初期化と同じで Hash.new() や Hash.new {} で定義出来るが内容は配列と同じ
+# 中でも便利そうだったのが特定のハッシュキーと値の初期化を行なう Hash.new {} 記法
+h = Hash.new { |hash, key| hash[key] = 'hello' }
+p h[:foo]
+p h[:bar]
+p h # foo,barに初期値のhelloが格納されている
