b611b54a8c95c2721ca9438cf9422524ae587f9c
koolii <kuriyama@createentrance.com>
2018-04-12 13:12:07 +0200

    :+1: fin until chapter 7.3

--- src/chapter07.rb
+++ src/chapter07.rb
@@ -1,7 +1,11 @@
 class User
   attr_reader :first_name, :last_name, :age
 
-  def initialize(first_name, last_name, age)
+  DEFAULT_AGE = 0
+  DEFAULT_LAST_NAME = '(unknown)'
+
+  # オーバーロードはできないみたい、引数が１つのinitializeを作成しようとしたが出来なかった
+  def initialize(first_name, last_name = DEFAULT_LAST_NAME, age = DEFAULT_AGE)
     @first_name = first_name
     @last_name = last_name
     @age = age
@@ -10,9 +14,20 @@ class User
   def full_name
     # クラス内でのインスタンス変数の参照は@を付ける付けないに関わらないらしい
     # ローカル変数が重複した時に@を付けることで区別が可能なのでそれは便利
-    p first_name.object_id
-    p @first_name.object_id
+    # p first_name.object_id
+    # p @first_name.object_id
     "#{first_name} #{last_name}"
+  end
+
+  # self.を付けるとクラスメソッドになる
+  def self.create_users(names)
+    names.map do |name|
+      User.new(name, '', 0)
+    end
+  end
+
+  def hello
+    puts "HELLO!! This is my information => Name: #{full_name}, Age: #{@age}"
   end
 end
 
@@ -22,5 +37,11 @@ users << User.new('Alice', 'Ruby', 20)
 users << User.new('Bob', 'Python', 50)
 
 users.each do |user|
-  puts "Name: #{user.full_name}, Age: #{user.age}"
+  user.hello
+end
+
+names = ['Alice', 'Bob', 'Carol']
+users = User.create_users(names)
+users.each do |user|
+  user.hello
 end
