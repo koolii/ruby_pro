5cd2672c4c4c3d00207db671fa2789d8e0f59dc3
koolii <kuriyama@createentrance.com>
2018-05-01 08:51:48 +0200

    :+1: chapter 9

--- src/chapter09.rb
+++ src/chapter09.rb
@@ -35,3 +35,64 @@ end
 puts 'end'
 
 
+# 9.3
+def currency_of(country)
+  case country
+  when :japan
+    'yen'
+  when :us
+    'dollar'
+  when :india
+    'rupee'
+  else
+    raise "invalid country. #{country}"
+    raise ArgumentError, "invalid country. #{country}" # Errorを指定することも可能
+  end
+end
+
+begin
+  p currency_of(:japan)
+  p currency_of(:italy)
+rescue => e
+  p e.message
+end
+
+# 9.6
+begin
+  # ファイルが存在しない場合は作成もしてくれるらしい
+  file = File.open('some.txt', 'w')
+  file << 'hi'
+rescue
+  p 'no file'
+ensure
+  p 'close'
+  file.close
+end
+
+begin
+  # ブロックで記述すればfile.closeを実行しなかったとしてもクローズ処理をしてくれる
+  File.open('some.txt', 'w') do |file|
+    file << 'Hello'
+    1 / 0
+  end
+rescue
+end
+
+# 例外が発生しなかったときの処理も記述することが出来る
+begin
+  p 'hello'
+  # 通常どおりに成功した場合はbegin節の最後の式が戻り値になる
+
+  raise RuntimeError, 'test'
+rescue
+  p '例外が発生しました'
+  # 例外が発生した場合はrescue節の最後の式が戻り値になる
+  # rescue内で単純にraiseだけを行なうと、rescueした例外をそのまま再度raiseすることになる
+  raise
+else
+  p '例外が発生しませんでした'
+ensure
+  p '終了'
+  # もしもensure節にreturnを書いてしまうと、
+  # 正常異常終了にかかわらずreturnで指定したものが帰ってしまうので注意
+end
